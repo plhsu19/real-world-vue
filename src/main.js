64cfdc089696e1ab15951586d662e9068e53079d
Pei-Lun Hsu <pei-lun.hsu@Admins-MBP.home>
2021-12-01 13:51:58 +0100

    create project app for vue-mastery:real world vue course

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import App from "./App.vue"; //root component
+import router from "./router"; // for vue router
+import store from "./store"; // for vuex
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App), // render the root component App
+}).$mount("#app"); // mount it to the DOM where id: app is, ie, the component with the id: app
