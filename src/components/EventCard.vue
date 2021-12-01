d4fdbbf2577a5d8ec3e72a1e358f6a337c089abe
Pei-Lun Hsu <pei-lun.hsu@Admins-MBP.home>
2021-12-01 15:32:46 +0100

    create a component: EventCard

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 @click="handler(arg, event)">test</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    propName: {
+      type: Number,
+    },
+  },
+  data() {
+    return {
+      key: value,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
