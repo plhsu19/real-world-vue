7990671abc9f79f2f62eed181cf08ec8dd7974f0
Pei-Lun Hsu <peilun.hsu@getyourguide.com>
2021-12-14 14:57:38 +0100

    add BaseIcon component, globally register BaseIcon

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue"; //root component
 import router from "./router"; // for vue router
 import store from "./store"; // for vuex
+import BaseIcon from '@/components/BaseIcon.vue';
 
 Vue.config.productionTip = false;
+
+Vue.component('BaseIcon', BaseIcon);
 
 new Vue({
   router,
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Show Event#1</h1>
+  <div>
+    <h1>showing event # {{id}}</h1>
+  </div>
 </template>
+
+<script>
+  export default {
+   props: ['id'], 
+  }
+</script>
+
+<style scoped>
+
+</style>
