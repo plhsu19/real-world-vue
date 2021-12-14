3920fcf365169b3fc2136343f9defc600c9e277f
Pei-Lun Hsu <peilun.hsu@getyourguide.com>
2021-12-14 15:39:27 +0100

    finish L7, complete base-icon component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,7 +3,7 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <base-icon />
+      <base-icon name="users" width="24" height="24"/>
       <span>{{ event.attendees.length }} attending</span>
     </div>
   </router-link>
