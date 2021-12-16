f51f8c906b19748fea434ae2bd19bebdaa1229b8
Pei-Lun Hsu <peilun.hsu@getyourguide.com>
2021-12-16 17:12:44 +0100

    access state, using getters, mapStates & mapGetters

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,36 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: 'abc123',
+      name: 'Adam Jahr',
+    },
+    categories: [
+      'sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ],
+  },
   mutations: {},
   actions: {},
+  getters: {
+    catLength: (state) => {
+      return state.categories.length;
+    },
+    doneTodos: (state) => {
+      return state.todos.filter(todo => todo.done);
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length;
+    },
+    getTodoById: (state) => (id) => {
+      return state.todos.find((todo) => todo.id === id)
+    }
+  },
   modules: {},
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>The event is created by user ID: {{ user.id }}</p>
+    <p>{{ getTodo(3) }}</p>
+    <p>{{ catCount }} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapGetters, mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState(["user", "categories"]),
+    ...mapGetters({
+      catCount: "catLength",
+      getTodo: "getTodoById",
+    }),
+  },
+};
+</script>
